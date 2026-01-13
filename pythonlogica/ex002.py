e08be320e81cb455d3e86f4d42eaf6785c0bb7e5
Gabriel Lacerda <lacerdaskylore@gmail.com>
2026-01-13 16:11:02 -0300

    Update ex002.py

--- pythonlogica/ex002.py
+++ pythonlogica/ex002.py
@@ -1,5 +1,4 @@
-#Descubra as duas notas do aluno e faça a média.
-nota = float(input('Digite a primeira nota: '))
+#Descubra as duas notas do aluno e faça a média.import 
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota + nota2)/2
 print('A media das suas duas notas é igual a: {}'.format(media))
